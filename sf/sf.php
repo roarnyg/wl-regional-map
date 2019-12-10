21d075ce3754579461ffa4316cfbc120b1f5b584
Iver Odin Kvello <iver@wp-hosting.no>
2019-12-10 20:37:35 +0100

    Support SF and Nordnorge both

---
+++ sf/sf.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+?>
+ <div id="sfMap" class="main-map">
+  <?php include(dirname(__FILE__) . "/sf.svg"); ?>
+ </div>
